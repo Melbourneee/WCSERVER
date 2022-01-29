1cdba0cd252d06784849408cf80407eb86f17395
Melbourneee <98503742+Melbourneee@users.noreply.github.com>
2022-01-29 21:04:44 +0800

    Add files via upload

---
+++ Demos/demo1.js
@@ -0,0 +1,12 @@
+//Gantan Melbourne
+//2075 - WC SERVER
+//Creating a Function
+
+function Display(x){
+  console.log(x)
+}
+
+Display(100);
+
+
+
---
+++ Demos/demo2.js
@@ -0,0 +1,14 @@
+//Gantan Melbourne
+//2075 - WC SERVER
+//Load and Use Core
+
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+//code here
+
+
+
+});
+
+server.listen(5000);
